379d7a038c112bf59c5a3c1febe95e7846cab104
Ananstasia Shulga <anastasiansh@gmail.com>
2022-03-20 22:00:19 +0400

    F-04 Загрузка моделей на страницу. Пагинация.  Валидация изображений. Изменение цены.

--- src/service/MainService.js
+++ src/service/MainService.js
@@ -9,6 +9,14 @@ export default {
         if (cityId) {
             const response = await Api.get(`/db/point/?cityId=${cityId}`);
             return response.data.data;
-        }        
+        }
+    },
+    async getCategories() {
+        const response = await Api.get(`/db/category`);
+        return response.data.data;
+    },
+    async getProducts(params) {
+        const response = await Api.get(`/db/car/`, {params});
+        return response.data;
     },
 }
