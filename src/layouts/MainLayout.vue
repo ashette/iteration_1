d4a9cf74362e89ee02ae581d64f95247b634b9f9
Ananstasia Shulga <anastasiansh@gmail.com>
2022-04-14 00:08:22 +0400

    deploy

--- src/layouts/MainLayout.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<style lang="scss">
-@import "@/scss/index.scss";
-</style>
--- src/views/admin/CarDetails.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="admin-content">
-    <h1>Карточка автомобиля</h1>
-  </div>
-</template>
-
